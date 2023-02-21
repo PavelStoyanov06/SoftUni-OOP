7831a1e4c1f2b2d51d1a075bf01ffd9c69551d31
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2023-02-21 21:56:00 +0200

    the new inheritance lab

---
+++ Solutions/InheritanceLab/CustomRandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random rand = new Random();
+            string toRemove = this[rand.Next(0, this.Count)];
+            this.Remove(toRemove);
+            return toRemove;
+        }
+    }
+}
