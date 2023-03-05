e8a0a861c295fa756325627c6bf81e758e699cce
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2023-03-05 23:01:28 +0200

    polymorphism lab

---
+++ Solutions/PolymorphismLab/02.Animals/Program.cs
@@ -0,0 +1,16 @@
+﻿using _02.Animals.Models;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Animal cat = new Cat("Peter", "Whiskas");
+            Animal dog = new Dog("George", "Meat");
+
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+        }
+    }
+}
